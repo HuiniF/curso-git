735ed8cb68a02e7afed5208432260bd74e5a8708
Huini Forte <huiniforte2@hotmail.com>
2021-09-18 01:40:43 -0300

    Acrescentada a pergunta como voce estah hj

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace TesteGit
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
         }
     }
 }
